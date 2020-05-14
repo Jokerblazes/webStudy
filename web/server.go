23258c8cd546c1f218c6cbd14dc62d1d9b04a1c5
joker <cdzheng@thoughtworks.com>
2020-05-14 17:27:24 +0800

    impl

--- web/server.go
+++ web/server.go
@@ -8,20 +8,19 @@ import (
 
 type Server struct {
 	Engine *gin.Engine
-	Car    *car.Service
+	Car    car.Service
 }
 
-func NewServer(car *car.Service) *Server {
+func NewServer(car car.Service) *Server {
 	server := &Server{Car: car}
-	server.Engine = setupRouter()
+	server.Engine = server.setupRouter()
 	return server
 }
 
-func setupRouter() *gin.Engine {
+func (server *Server) setupRouter() *gin.Engine {
 	r := gin.Default()
 
-	// Get user value
-	r.GET("/car/:number", func(c *gin.Context) {
+	r.GET("/cars", func(c *gin.Context) {
 		number := c.Params.ByName("number")
 		c.JSON(http.StatusOK, gin.H{"number": number, "name": "车名"})
 	})
