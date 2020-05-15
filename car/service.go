855ad0c4293f460a4ff33b89df45258672d4f361
joker <cdzheng@thoughtworks.com>
2020-05-15 10:03:46 +0800

    add save method

--- car/service.go
+++ car/service.go
@@ -4,9 +4,15 @@ type Service interface {
 	Cars() []Car
 
 	Car(id CardId) Car
+
+	save(car Car) error
 }
 
 type service struct {
+}
+
+func (svc *service) save(car Car) error {
+	panic("implement me")
 }
 
 func (svc *service) Cars() []Car {
